308e46de339bc2d234e566caffea5e2fbbfb3656
Anwesh43 <anweshthecool0@gmail.com>
2021-06-18 02:53:38 +0530

    adding constants and libraries

--- balldiaglineview/src/main/java/com/example/balldiaglineview/BallDiagLineView.kt
+++ balldiaglineview/src/main/java/com/example/balldiaglineview/BallDiagLineView.kt
@@ -1,2 +1,26 @@
 package com.example.balldiaglineview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#01579B",
+    "#00C853",
+    "#FFC107",
+    "#2962FF"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val rFactor : Float = 5.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
